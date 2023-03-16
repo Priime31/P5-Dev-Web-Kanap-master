2da49399d749e9a1d49aead4a71a53fd1a27f35b
arthur willem <arthurwillem13@hotmail.com>
2023-03-16 02:25:41 +0100

    fix[product.js]: fix the indenting of product to cart and finish productJS.

--- front/js/product.js
+++ front/js/product.js
@@ -6,12 +6,12 @@ const id = url.searchParams.get("id");
 
 //récupère dans l'API les données du produit correspondant à l'id de la page au format JSON.
 const getProductsById = async () => {
-    const reponseId = await fetch('http://localhost:3000/api/products/' + id);
+    const reponseId = await fetch("http://localhost:3000/api/products/" + id);
     return await reponseId.json();
-}
+};
 
 //créer du contenu HTML avec les données retournées par l'API.
-const generateProducts = (products) => {
+const generateProducts = (product) => {
     const itemImg = document.querySelector(".item__img");
     const itemName = document.querySelector("#title");
     const itemPrice = document.querySelector("#price");
@@ -33,13 +33,13 @@ const generateProducts = (products) => {
 
         itemColor.appendChild(colorOption);
     }
-}
+};
 
 //stock les données liées à l'id du produit dans une constante puis créer le HTML.
 const start = async () => {
     const product = await getProductsById();
     generateProducts(product);
-}
+};
 
 start();
 
@@ -49,49 +49,18 @@ addButton.addEventListener("click", function pushCart() {
     //stock dans la variable "cart" soit le panier déjà présent dans le localStorage soit s'il n'y a rien, un panier vide.
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
     let color = document.getElementById("colors").value;
-    console.log(color);
-    let quantity = document.getElementById("quantity").value;
-    
-    //cherche un objet dans le panier correspondant aux valeurs id et color de la page produit 
-    const indentObject = (id, color, quantity) => {
-        
-/*
-        const toto = cart.map((product) => {
-            console.log(product.quantity);
-            product.quantity++;
-            console.log(product.quantity);
-            return product;
-        });*/
-        //console.log(toto);
+    let quantity = parseInt(document.getElementById("quantity").value);
 
+    /*cherche un objet dans le panier correspondant aux valeurs id et color de la page produit puis s'il y a une correspondance, incrémente
+    la quantité selectionnée par l'utilisateur, sinon créer l'objet dans le panier.*/
 
-        for (let product of cart) {
-            console.log(product);
-            if (product.id === id && product.coulor === color) {
-                console.log("id et color dans le panier");
-                /*const cartQuantity = parseInt(product["quantity"]);
-                const objectQuantity = parseInt(quantity);*/
-
-                product.quantity = product.quantity + parseInt(quantity);
-
-            } else {
-                console.log("non présent dans le panier");
-                if (color !== "" && quantity !== 0) {
-                    let cartObject = {
-                        id: id,
-                        coulor: color,
-                        quantity: parseInt(quantity),
-                    };
-                
-                    cart.push(cartObject);
-                
-                    
-                }
+    const checkValues = () => {
+        for (let item of cart) {
+            if (item.id === id && item.color === color) {
+                return true;
             }
-            
         }
-        localStorage.setItem("cart", JSON.stringify(cart));
-    }
+    };
 
     if (cart.length === 0) {
         if (color !== "" && quantity != 0) {
@@ -100,32 +69,31 @@ addButton.addEventListener("click", function pushCart() {
                 color: color,
                 quantity: parseInt(quantity),
             };
-    
+
             cart.push(cartObject);
-    
+
             localStorage.setItem("cart", JSON.stringify(cart));
         }
+    } else if (checkValues() === true) {
+        let indentObject = cart.map((product) => {
+            if (product.id === id && product.color === color) {
+                product.quantity += quantity;
+            }
+            return product;
+        });
+        cart = indentObject;
+        localStorage.setItem("cart", JSON.stringify(cart));
     } else {
-        indentObject(id, color, quantity);
+        if (color !== "" && quantity != 0) {
+            let cartObject = {
+                id: id,
+                color: color,
+                quantity: parseInt(quantity),
+            };
+
+            cart.push(cartObject);
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
     }
-
-    console.log(cart);
 });
-
-
-
-
-
-
-/**
- * mon but maintenant est de vérifier si les values (id / couleurs) selectionnées par le client sont déjà présentes dans le localstorage
- * et si oui, les incrémenter aux précédentes plutôt que les dupliquer.
- * 
- * j'ai besoin de :
- * 
- * - données du localstorage (id / couleurs),
- * - données selectionnées par le client (quantité / couleurs),
- * - d'une boucle qui check les données du panier,
- * - d'une fonction qui ajoute les données renvoyées par le client au panier (si elles sont déjà présentes),
- * - ca se fasse avant le localStorage.setItem,
- */
